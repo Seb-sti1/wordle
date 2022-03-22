2ea25c6dde6b7582ba0c7b24a6dcb1dae4ce40ef
Seb-asti1 <65665540+seb-sti1@users.noreply.github.com>
2022-03-22 23:42:56 +0100

    basic structure

---
+++ main.c
@@ -0,0 +1,43 @@
+
+
+
+int main(int argc, char const *argv[])
+{
+    int tries = 0;
+
+    // load dict
+
+    // choose a random word
+
+    while (tries < 6) {
+
+        // ask word
+
+        // check for word in dict
+
+        // if everything ok
+            // compare with the words
+                // if its the same -> You win
+                // else print the differences
+
+    }
+
+    // if too many tries -> you lose
+    
+    return 0;
+}
+
+
+
+/**
+ * ask for a string of 5 char
+ * 
+ * return : char* a string of 5 char or NULL (on error)
+ * 
+ */
+char* askWord()
+{
+
+
+
+}
---
+++ mots.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stddef.h>
+
+
+/**
+ * Load the dict into a global static variable
+ * 
+ * 
+ * arg:
+ * char* filepath : file path
+ * 
+ * return: 
+ * int (bool) success
+ * 
+ * 
+ */
+int loadDict(char* filepath)
+{
+    // load file
+
+    // save in a ordered struct 
+
+}
+
+/**
+ * Search a word in the dictonary
+ * 
+ * arg:
+ * char* word : the word to search
+ * 
+ * return:
+ * int (bool) found 
+ * 
+ */
+int searchDict(char* word)
+{
+    // make something quick
+}
+
+
+/**
+ * Search for a random word
+ * 
+ * return:
+ * a random word
+ * 
+ */
+char* randomWord()
+{
+    // choose a random word
+
+}
